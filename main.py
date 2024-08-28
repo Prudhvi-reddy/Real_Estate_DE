baf7eb576831eeba7363f9c9efe14402964de6d1
prudhvi-reddy <prudhvi.shine@gmail.com>
2024-08-28 19:26:31 -0400

    web scraper + brightdata

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+SBR_WS_CDP = 'wss://brd-customer-hl_516250ae-zone-real_estate:8j45zl1javmq@brd.superproxy.io:9222'
+BASE_URL = "https://www.redfin.com"
+LOCATION = "London"
+
+async def run(pw):
+    print('Connecting to Scraping Browser...')
+    browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
+    try:
+        page = await browser.new_page()
+        print('Connected! Navigating to {BASE_URL}')
+        await page.goto(BASE_URL)
+       
+        print('Navigated! Scraping page content...')
+        html = await page.content()
+        print(html)
+    finally:
+        await browser.close()
+
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
