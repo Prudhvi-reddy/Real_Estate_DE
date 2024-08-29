2a0eeb7939d206c3da3b4456e056aa4c60b3acdd
prudhvi-reddy <prudhvi.shine@gmail.com>
2024-08-29 18:22:14 -0400

    get details of property by using bs4

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from playwright.async_api import async_playwright
 
 SBR_WS_CDP = 'wss://brd-customer-hl_516250ae-zone-real_estate:8j45zl1javmq@brd.superproxy.io:9222'
 BASE_URL = "https://www.redfin.com"
-LOCATION = "London"
+LOCATION = "Orlando"
 
 async def run(pw):
     print('Connecting to Scraping Browser...')
@@ -12,7 +12,17 @@ async def run(pw):
         page = await browser.new_page()
         print('Connected! Navigating to {BASE_URL}')
         await page.goto(BASE_URL)
-       
+   
+   
+   
+    # Fill  search bar with location
+        await page.fill('input[name="searchInputBox"]'), LOCATION
+        await page.keyboard.press("Enter")
+        print("waiting for search results..........")
+        await page.wait_for_load_state("load")
+        
+
+
         print('Navigated! Scraping page content...')
         html = await page.content()
         print(html)
